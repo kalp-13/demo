ab3f1d7ab732e4d4f57020dff1cabac5bc4dd3d0
DELL USER <DELL USER@DESKTOP-JSLVUL9>
2023-01-10 15:14:31 +0530

    my second commit

---
+++ Jenkins/src/main/java/Demo/Test3.java
@@ -0,0 +1,13 @@
+package Demo;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test3 {
+	@Test
+	public void m3()
+	{
+		System.out.println("hello ji");
+	}
+
+}
